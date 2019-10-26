8f98958b634e63e32814689a19b02d91c1485bc7
Gijs <gijs@MacBook-Pro-di-Gebruiker.local>
2019-10-26 12:12:36 +0200

    react landing page

---
+++ src/components/Landing.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Say hello to ReactJS</h1>
+      <p>
+        You will learn a Frontend framework from scratch, to become a Ninja
+        Developer
+      </p>
+      <button className="">Awesome!</button>
+      <h2>Declarative</h2>
+      <p>React makes it painless to create interactive UIs.</p>
+      <h2>Components</h2>
+      <p>Build encapsuated components that manage their state.</p>
+      <h2>Single Way</h2>
+      <p>A set of immutable values are passed to the components.</p>
+      <h2>JSX</h2>
+      <p>Statically-typed, designed to run on modern browsers.</p>
+    </div>
+  );
+};
+
+export default Landing;
